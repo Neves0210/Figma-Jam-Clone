3e0ad48b955591f2d57bc8b96c330843bd51bbaa
Neves0210 <gabriel@up4tech.com.br>
2023-07-12 14:03:08 -0300

    Geração dos Primeiros Quadrados Aparentes em Tela

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,45 @@ import ReactFlow , { Background, Controls }from 'reactflow';
 import {zinc} from  "tailwindcss/colors"
 import 'reactflow/dist/style.css';
 
+import { Square } from './components/nodes/Square';
+
+
+const NODE_TYPES ={
+  square: Square,
+}
+
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 1000,
+      y: 400,
+    },
+    data: {},
+  },
+
+] satisfies Node[]
+
 function App() {
   return (
     <div className='w-screnn h-screen'>
-    <ReactFlow>
+    <ReactFlow
+      nodeTypes={NODE_TYPES}
+      nodes={INITIAL_NODES}
+      >
+
       <Background
         gap={12}
         size={2}
